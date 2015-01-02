a9ad4e6313e07ffe2271fc6378b968feacacbd44
jakegough <jakegough@gmail.com>
2015-01-01 21:30:07 -0700

    updated nuget build
    added multiple frameworks:
    netmf 3.0, 4.0, 4.1, 4.2
    net 3.0, 3.5, 4.0, 4.5

--- Build.NuGet/NuGet-Package.bat
+++ Build.NuGet/NuGet-Package.bat
@@ -1,5 +1,15 @@
 call NuGet-Settings.cmd
 
-"%NUGET_EXE%" pack "%NUGET_PROJECT%" -Build -Properties Configuration=%NUGET_CONFIGURATION% -Properties Platform=AnyCPU
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Nuget-Clean
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget Net 3.0"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget Net 3.5"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget Net 4.0"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget Net 4.5"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget NetMF 3.0"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget NetMF 4.0"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget NetMF 4.1"
+"%MSBUILD_EXE%" %NUGET_PROJECT% /t:Build /p:Configuration="Nuget NetMF 4.2"
+
+"%NUGET_EXE%" pack "%NUGET_PROJECT%" -Verbosity detailed -Build -Properties Configuration="Nuget Net 4.0"
 
 pause
